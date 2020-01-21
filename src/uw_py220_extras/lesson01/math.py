7fe450bfa5bcb3a06157da563153e1d668b14cf1
Andy Miles <akmiles@icloud.com>
2020-01-21 07:36:09 -0800

    changed to class based example

--- src/uw_py220_extras/lesson01/math.py
+++ src/uw_py220_extras/lesson01/math.py
@@ -1,6 +1,13 @@
-def addit(lhs, rhs):
-    return lhs + rhs
+class AdvancedMath():
+
+    def __init__(self, blah):
+        pass
+
+    def addit(self, lhs, rhs):
+        # normally lots of complex stuff here
+        return lhs + rhs
 
 
-def subit(lhs, rhs):
-    return lhs - rhs
+    def subit(self, lhs, rhs):
+        # and here
+        return lhs - rhs
--- tests/lesson01/test_math.py
+++ tests/lesson01/test_math.py
@@ -2,10 +2,12 @@ from uw_py220_extras.lesson01 import math
 
 
 def test_addit():
-    total = math.addit("c", "a")
+    advanced_math = math.AdvancedMath("blah")
+    total = advanced_math.addit("c", "a")
     assert total == "ca"
 
 
 def test_subit():
-    total = math.subit(9, 7)
+    advanced_math = math.AdvancedMath("more blah")
+    total = advanced_math.subit(9, 7)
     assert total == 2
